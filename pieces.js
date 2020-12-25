6ee818894ec6b8ff003eaa2491d1a59e23182bdf
Neelesh Mishra <neeleshojmishra@gmail.com>
2020-12-25 10:24:03 +0530

    Major updates

--- pieces.js
+++ pieces.js
@@ -1,14 +1,23 @@
 function posToIndex(pos) {
-  return p5.Vector.div(pos, tileSize);
+  let n = p5.Vector.div(pos, tileSize)
+  return createVector(n.y, n.x);
 }
 
 class Piece {
   constructor(x, y, isWhite) {
     this.pos = p5.Vector.mult(createVector(x, y), tileSize);
-    this.prevPos = createVector(0, 0);
     this.isWhite = isWhite;
 
+    this.updatePrevPos();
+
+    if (this.isWhite) {
+      this.king = kingw;
+    } else {
+      this.king = kingb;
+    }
+
     this.lifted = false;
+    this.hasMoved = false;
   }
 
   mouseOver() {
@@ -16,9 +25,13 @@ class Piece {
       this.pos.y < mouseY && mouseY < this.pos.y + tileSize);
   }
 
-  lift() {
-    this.lifted = true;
+  updatePrevPos() {
     this.prevPos = createVector(this.pos.x, this.pos.y);
+  }
+
+  lift() {
+    console.log(this.king);
+    this.lifted = true;
   }
 
   drop() {
@@ -28,13 +41,16 @@ class Piece {
       floor(mouseY / tileSize) * tileSize
     );
 
-    if (isBoardValidMove(p) && this.isValidMove(p)) {
+    let notOffScreen = (0 <= p.x && p.x <= width && 0 <= p.y && p.y <= height);
+
+    if (this.isValidMove(p) && notOffScreen) {
       this.pos = p;
+      this.hasMoved = true;
     } else {
       this.pos = this.prevPos;
     }
 
-    return this.pos;
+    return [this.prevPos, this.pos];
   }
 
   show() {
@@ -55,6 +71,7 @@ class Piece {
 
     strokeWeight(4);
     textAlign(CENTER, CENTER);
+
     text(this.letter, this.pos.x + tileSize / 2, this.pos.y + tileSize / 2);
   }
 }
@@ -65,8 +82,222 @@ class Rook extends Piece {
     this.letter = 'R';
   }
 
-  isValidMove(pos) {
-    let pos = posToIndex(pos);
+  isValidMove(a, b) {
+    let goto;
+    if (a instanceof p5.Vector) goto = posToIndex(a);
+    else goto = createVector(a, b);
+    let current = posToIndex(this.prevPos);
+
+    // if (pos.x == tp.x || pos.y == tp.y) return true;
+
+    // right
+    for (let i = current.y; i < 8; i++) {
+      let t = board[current.x][i];
+      if (t == this) continue;
+
+      if (t == '') {
+        if (goto.equals(createVector(current.x, i))) return true;
+      } else if (t.isWhite != this.isWhite) { // not same color
+        if (goto.equals(createVector(current.x, i))) return true;
+        break;
+      } else {
+        break;
+      }
+    }
+
+    // left
+    for (let i = current.y; i >= 0; i--) {
+      let t = board[current.x][i];
+      if (t == this) continue;
+
+      if (t == '') {
+        if (goto.equals(createVector(current.x, i))) return true;
+      } else if (t.isWhite != this.isWhite) { // not same color
+        if (goto.equals(createVector(current.x, i))) return true;
+        break;
+      } else {
+        break;
+      }
+    }
+
+    // up
+    for (let i = current.x; i >= 0; i--) {
+      let t = board[i][current.y];
+      if (t == this) continue;
+
+      if (t == '') {
+        if (goto.equals(createVector(i, current.y))) return true;
+      } else if (t.isWhite != this.isWhite) { // not same color
+        if (goto.equals(createVector(i, current.y))) return true;
+        break;
+      } else {
+        break;
+      }
+    }
+
+    // down
+    for (let i = current.x; i < 8; i++) {
+      let t = board[i][current.y];
+      if (t == this) continue;
+
+      if (t == '') {
+        if (goto.equals(createVector(i, current.y))) return true;
+      } else if (t.isWhite != this.isWhite) { // not same color
+        if (goto.equals(createVector(i, current.y))) return true;
+        break;
+      } else {
+        break;
+      }
+    }
+
+    return false;
+  }
+}
+
+class Pawn extends Piece {
+  constructor(x, y, isWhite) {
+    super(x, y, isWhite);
+    this.letter = 'P';
+  }
+
+  isValidMove(a, b) {
+    let goto;
+    let current = posToIndex(this.prevPos);
+
+    if (a instanceof p5.Vector) {
+      goto = posToIndex(a);
+    } else goto = createVector(a, b);
+
+    if ((!this.isWhite && current.x >= 7) || (this.isWhite && current.x <= 0)) {
+      return false;
+    }
+
+    if (this.isWhite) {
+      if (goto.equals(createVector(current.x - 1, current.y)) && board[current.x - 1][current.y] == '') return true;
+      if (!this.hasMoved)
+        if (goto.equals(createVector(current.x - 2, current.y)) && board[current.x - 2][current.y] == '') return true;
+
+      let t = board[current.x - 1][current.y - 1];
+      if (t != null && t != '' && !t.isWhite) {
+        if (goto.equals(createVector(current.x - 1, current.y - 1))) return true;
+      }
+
+      t = board[current.x - 1][current.y + 1];
+      if (t != null && t != '' && !t.isWhite) {
+        if (goto.equals(createVector(current.x - 1, current.y + 1))) return true;
+
+      }
+    } else {
+      if (goto.equals(createVector(current.x + 1, current.y)) && board[current.x + 1][current.y] == '') return true;
+      if (!this.hasMoved)
+        if (goto.equals(createVector(current.x + 2, current.y)) && board[current.x + 2][current.y] == '') return true;
+
+      let t = board[current.x + 1][current.y - 1];
+      if (t != null && t != '' && t.isWhite) {
+        if (goto.equals(createVector(current.x + 1, current.y - 1))) return true;
+      }
+
+      t = board[current.x + 1][current.y + 1];
+      if (t != null && t != '' && t.isWhite) {
+        if (goto.equals(createVector(current.x + 1, current.y + 1))) return true;
+      }
+    }
+
+    return false;
+  }
+}
+
+class Bishop extends Piece {
+  constructor(x, y, isWhite) {
+    super(x, y, isWhite);
+    this.letter = 'B';
+  }
+
+  isValidMove(a, b) {
+    let goto;
+    let current = posToIndex(this.prevPos);
+
+    if (a instanceof p5.Vector) {
+      goto = posToIndex(a);
+    } else goto = createVector(a, b);
+
+    for (let i = 0; i < 8; i++) {
+      let a = current.x - i;
+      let b = current.y - i;
+
+      if (a < 0 || b < 0) break;
+
+      let t = board[a][b];
+      if (t == this) continue;
+
+      if (t == '') {
+        if (goto.equals(createVector(a, b))) return true;
+      } else if (t.isWhite != this.isWhite) {
+        if (goto.equals(createVector(a, b))) return true;
+        break;
+      } else {
+        break;
+      }
+    }
+
+    for (let i = 0; i < 8; i++) {
+      let a = current.x + i;
+      let b = current.y + i;
+
+      if (a > 7 || b > 7) break;
+
+      let t = board[a][b];
+      if (t == this) continue;
+
+      if (t == '') {
+        if (goto.equals(createVector(a, b))) return true;
+      } else if (t.isWhite != this.isWhite) {
+        if (goto.equals(createVector(a, b))) return true;
+        break;
+      } else {
+        break;
+      }
+    }
+
+    for (let i = 0; i < 8; i++) {
+      let a = current.x + i;
+      let b = current.y - i;
+
+      if (a > 7 || b > 7 || a < 0 || b < 0) break;
+
+      let t = board[a][b];
+      if (t == this) continue;
+
+      if (t == '') {
+        if (goto.equals(createVector(a, b))) return true;
+      } else if (t.isWhite != this.isWhite) {
+        if (goto.equals(createVector(a, b))) return true;
+        break;
+      } else {
+        break;
+      }
+    }
+
+    for (let i = 0; i < 8; i++) {
+      let a = current.x - i;
+      let b = current.y + i;
+
+      if (a > 7 || b > 7 || a < 0 || b < 0) break;
+
+      let t = board[a][b];
+      if (t == this) continue;
+
+      if (t == '') {
+        if (goto.equals(createVector(a, b))) return true;
+      } else if (t.isWhite != this.isWhite) {
+        if (goto.equals(createVector(a, b))) return true;
+        break;
+      } else {
+        break;
+      }
+    }
+
+    return false;
   }
 }
 
@@ -77,19 +308,150 @@ class Queen extends Piece {
     this.letter = 'Q';
   }
 
-  isValidMove(pos) {
-    return true;
-  }
-}
+  isValidMove(a, b) {
+    let goto;
+    let current = posToIndex(this.prevPos);
 
-class Pawn extends Piece {
-  constructor(x, y, isWhite) {
-    super(x, y, isWhite);
-    this.letter = 'P';
-  }
+    if (a instanceof p5.Vector) {
+      goto = posToIndex(a);
+    } else goto = createVector(a, b);
 
-  isValidMove(pos) {
-    return true;
+    for (let i = current.y; i < 8; i++) {
+      let t = board[current.x][i];
+      if (t == this) continue;
+
+      if (t == '') {
+        if (goto.equals(createVector(current.x, i))) return true;
+      } else if (t.isWhite != this.isWhite) { // not same color
+        if (goto.equals(createVector(current.x, i))) return true;
+        break;
+      } else {
+        break;
+      }
+    }
+
+    // left
+    for (let i = current.y; i >= 0; i--) {
+      let t = board[current.x][i];
+      if (t == this) continue;
+
+      if (t == '') {
+        if (goto.equals(createVector(current.x, i))) return true;
+      } else if (t.isWhite != this.isWhite) { // not same color
+        if (goto.equals(createVector(current.x, i))) return true;
+        break;
+      } else {
+        break;
+      }
+    }
+
+    // up
+    for (let i = current.x; i >= 0; i--) {
+      let t = board[i][current.y];
+      if (t == this) continue;
+
+      if (t == '') {
+        if (goto.equals(createVector(i, current.y))) return true;
+      } else if (t.isWhite != this.isWhite) { // not same color
+        if (goto.equals(createVector(i, current.y))) return true;
+        break;
+      } else {
+        break;
+      }
+    }
+
+    // down
+    for (let i = current.x; i < 8; i++) {
+      let t = board[i][current.y];
+      if (t == this) continue;
+
+      if (t == '') {
+        if (goto.equals(createVector(i, current.y))) return true;
+      } else if (t.isWhite != this.isWhite) { // not same color
+        if (goto.equals(createVector(i, current.y))) return true;
+        break;
+      } else {
+        break;
+      }
+    }
+
+    for (let i = 0; i < 8; i++) {
+      let a = current.x - i;
+      let b = current.y - i;
+
+      if (a < 0 || b < 0) break;
+
+      let t = board[a][b];
+      if (t == this) continue;
+
+      if (t == '') {
+        if (goto.equals(createVector(a, b))) return true;
+      } else if (t.isWhite != this.isWhite) {
+        if (goto.equals(createVector(a, b))) return true;
+        break;
+      } else {
+        break;
+      }
+    }
+
+    for (let i = 0; i < 8; i++) {
+      let a = current.x + i;
+      let b = current.y + i;
+
+      if (a > 7 || b > 7) break;
+
+      let t = board[a][b];
+      if (t == this) continue;
+
+      if (t == '') {
+        if (goto.equals(createVector(a, b))) return true;
+      } else if (t.isWhite != this.isWhite) {
+        if (goto.equals(createVector(a, b))) return true;
+        break;
+      } else {
+        break;
+      }
+    }
+
+    for (let i = 0; i < 8; i++) {
+      let a = current.x + i;
+      let b = current.y - i;
+
+      if (a > 7 || b > 7 || a < 0 || b < 0) break;
+
+      let t = board[a][b];
+      if (t == this) continue;
+
+      if (t == '') {
+        if (goto.equals(createVector(a, b))) return true;
+      } else if (t.isWhite != this.isWhite) {
+        if (goto.equals(createVector(a, b))) return true;
+        break;
+      } else {
+        break;
+      }
+    }
+
+    for (let i = 0; i < 8; i++) {
+      let a = current.x - i;
+      let b = current.y + i;
+
+      if (a > 7 || b > 7 || a < 0 || b < 0) break;
+
+      let t = board[a][b];
+      if (t == this) continue;
+
+      if (t == '') {
+        if (goto.equals(createVector(a, b))) return true;
+      } else if (t.isWhite != this.isWhite) {
+        if (goto.equals(createVector(a, b))) return true;
+        break;
+      } else {
+        break;
+      }
+    }
+
+    return false;
   }
 }
 
@@ -97,21 +459,38 @@ class King extends Piece {
   constructor(x, y, isWhite) {
     super(x, y, isWhite);
     this.letter = 'K';
+
+    this.inCheck = [];
   }
 
-  isValidMove(pos) {
-    return true;
-  }
-}
+  isValidMove(a, b) {
+    let goto;
+    let current = posToIndex(this.prevPos);
 
-class Bishop extends Piece {
-  constructor(x, y, isWhite) {
-    super(x, y, isWhite);
-    this.letter = 'B';
-  }
+    if (a instanceof p5.Vector) {
+      goto = posToIndex(a);
+    } else goto = createVector(a, b);
 
-  isValidMove(pos) {
-    return true;
+    let arr = [
+      [-1, -1],
+      [-1, 0],
+      [-1, 1],
+      [0, -1],
+      [0, 1],
+      [1, -1],
+      [1, 0],
+      [1, 1],
+    ]
+
+    for (let i of arr) {
+      let a = i[0] + current.x;
+      let b = i[1] + current.y;
+      if (0 <= a && a < 8 && 0 <= b && b < 8 && board[a][b].isWhite != this.isWhite) {
+        if (goto.equals(createVector(a, b))) return true;
+      }
+    }
+
+    return false;
   }
 }
 
@@ -121,7 +500,34 @@ class Knight extends Piece {
     this.letter = 'N';
   }
 
-  isValidMove(pos) {
-    return true;
+  isValidMove(a, b) {
+    let goto;
+    let current = posToIndex(this.prevPos);
+
+    if (a instanceof p5.Vector) {
+      goto = posToIndex(a);
+    } else goto = createVector(a, b);
+
+    let arr = [
+      [-2, -1],
+      [-2, 1],
+      [-1, 2],
+      [-1, -2],
+
+      [2, -1],
+      [2, 1],
+      [1, 2],
+      [1, -2],
+    ]
+
+    for (let i of arr) {
+      let a = i[0] + current.x;
+      let b = i[1] + current.y;
+      if (0 <= a && a < 8 && 0 <= b && b < 8 && board[a][b].isWhite != this.isWhite) {
+        if (goto.equals(createVector(a, b))) return true;
+      }
+    }
+
+    return false;
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,47 +1,67 @@
 let tileSize = 70;
 let pickedPiece;
 
+let kingb;
+let kingw;
+
+// let board = [
+//   ['', '', '', '', '', '', '', ''],
+//   ['pb', 'pb', 'pb', 'pb', 'pb', 'pb', 'pb', 'pb'],
+//   ['', '', '', '', '', '', '', ''],
+//   ['', '', '', '', '', '', '', ''],
+//   ['', '', '', '', '', '', '', ''],
+//   ['', 'kw', '', '', '', '', '', ''],
+//   ['', '', 'qw', '', '', 'pw', 'bw', ''],
+//   ['rw', '', '', 'nw', '', '', '', '']
+// ];
+
+
 let board = [
-  ['', '', '', '', '', '', '', ''],
+  ['rb', 'nb', 'bb', 'qb', 'kb', 'bb', 'nb', 'rb'],
   ['pb', 'pb', 'pb', 'pb', 'pb', 'pb', 'pb', 'pb'],
   ['', '', '', '', '', '', '', ''],
   ['', '', '', '', '', '', '', ''],
   ['', '', '', '', '', '', '', ''],
   ['', '', '', '', '', '', '', ''],
-  ['', '', '', '', '', '', '', ''],
-  ['rw', '', '', '', '', '', '', '']
+  ['pw', 'pw', 'pw', 'pw', 'pw', 'pw', 'pw', 'pw'],
+  ['rw', 'nw', 'bw', 'qw', 'kw', 'bw', 'nw', 'rw']
 ];
 
-// let board = [
-//   ['rb', 'nb', 'bb', 'qb', 'kb', 'bb', 'nb', 'rb'],
-//   ['pb', 'pb', 'pb', 'pb', 'pb', 'pb', 'pb', 'pb'],
-//   ['', '', '', '', '', '', '', ''],
-//   ['', '', '', '', '', '', '', ''],
-//   ['', '', '', '', '', '', '', ''],
-//   ['', '', '', '', '', '', '', ''],
-//   ['pw', 'pw', 'pw', 'pw', 'pw', 'pw', 'pw', 'pw'],
-//   ['rw', 'nw', 'bw', 'qw', 'kw', 'bw', 'nw', 'rw']
-// ];
-
-let human = 'w';
-let ai = 'b';
-let currentPlayer = 'w';
-
-function isBoardValidMove(pos) {
-  return true;
-}
-
+let history = [board];
 
 function setup() {
   createCanvas(tileSize * 8, tileSize * 8);
   initBoard();
+  angleMode(DEGREES);
 }
 
 function draw() {
   background(255);
 
+  kingb.inCheck = [];
+  kingw.inCheck = [];
+
+  // TODO: when the king is in check:
+  // 1. Move the king from the tile
+  //       - If there is more than one check
+  // 2. Kill the piece which is giving the check
+  //       - With another piece
+  //       - With the king
+  //            . If the piece isn't backed up by another piece of the same color
+  //            . If there is only one check
+  // 3. Block the piece with another piece
+
+  checkForCheck();
+
   drawGrid();
   showAllPieces();
+  highlightAvailableMoves();
+
+}
+
+function keyPressed() {
+  // if (key == 's') console.log(board);
+  if (key == 's') console.log(history);
 }
 
 function mousePressed() {
@@ -56,9 +76,35 @@ function mousePressed() {
   }
 }
 
+function undoBoard() {
+  if (history >= 2) {
+    board = history[history.length - 2];
+    history.splice(history.length - 1, 1);
+    initBoard();
+  }
+}
+
 function mouseReleased() {
   if (pickedPiece != null) {
-    let pos = pickedPiece.drop();
+
+    let p = pickedPiece.drop();
+
+    let p1 = posToIndex(p[0]);
+    let p2 = posToIndex(p[1]);
+
+
+    if (pickedPiece instanceof Pawn) {
+      if (p2.x == 0 || p2.x == 7) {
+        pickedPiece = new Queen(p2.y, p2.x, pickedPiece.isWhite);
+      }
+    }
+
+    board[p1.x][p1.y] = '';
+    board[p2.x][p2.y] = pickedPiece;
+
+    pickedPiece.updatePrevPos();
+
+    history.push(boardToString(board));
     pickedPiece = null;
   }
 }
@@ -81,7 +127,66 @@ function initBoard() {
         if (type == 'p') c = Pawn;
 
         board[i][j] = new c(j, i, color == 'w');
+        if (c == King) {
+          if (color == 'w') {
+            kingw = board[i][j]
+          } else {
+            kingb = board[i][j];
+          }
+        }
       }
+    }
+  }
+}
+
+function boardToString(board) {
+  let b = [];
+  for (let i = 0; i < 8; i++) {
+    let arr = [];
+    for (let j = 0; j < 8; j++) {
+      let s = '';
+      let t = board[i][j];
+
+      if (t == '') {
+        arr.push('');
+        continue;
+      }
+
+      if (t instanceof Pawn) s = 'p';
+      if (t instanceof Queen) s = 'q';
+      if (t instanceof Rook) s = 'r';
+      if (t instanceof King) s = 'k';
+      if (t instanceof Knight) s = 'n';
+      if (t instanceof Bishop) s = 'b';
+
+      s += (t.isWhite ? 'w' : 'b');
+      arr.push(s);
+    }
+    b.push(arr);
+  }
+  return b;
+}
+
+function highlightAvailableMoves() {
+  fill(125, 255, 0);
+  noStroke();
+  if (pickedPiece != null) {
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        if (pickedPiece.isValidMove(i, j)) circle(j * tileSize + tileSize / 2, i * tileSize + tileSize / 2, 20);
+      }
+    }
+  }
+}
+
+function checkForCheck() {
+  for (let t of getAllPieces()) {
+    if (t.isWhite && t.isValidMove(kingb.pos)) {
+      kingb.inCheck.push(t);
+    }
+
+    if (!t.isWhite && t.isValidMove(kingw.pos)) {
+      kingw.inCheck.push(t);
     }
   }
 }
@@ -99,11 +204,16 @@ function getAllPieces() {
 
 function showAllPieces() {
   for (let p of getAllPieces()) {
+    if (p instanceof King && p.inCheck) {
+      fill(255, 0, 0);
+      rect(p.pos.x, p.pos.y, tileSize, tileSize);
+    }
     p.show();
   }
 }
 
 function drawGrid() {
+  stroke(0);
   for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
       if (i % 2 != j % 2)
